6ac492befc7fa6949bafc653c58b7ff23b21271f
dianekapr <dianekapratiwi72@gmail.com>
2025-10-24 18:00:37 +0700

    layout

--- ulaman-ui-ref/tailwind.config.js
+++ ulaman-ui-ref/tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}', // If you're using Next.js app directory (for Next.js 13 and above)
+    './app/**/*.{js,ts,jsx,tsx}', 
   ],
   theme: {
     extend: {},
